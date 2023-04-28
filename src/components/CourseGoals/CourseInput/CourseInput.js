1533dcbfd8eb43c83cbdb10f31a8ed3f8d6ae9d2
Laxmi Prasanna <laxmiprasanna59@gmail.com>
2023-04-28 17:21:48 +0530

    setting dynamic inline styles

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,32 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    //reset the red inline styles for valid input
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    //add red inline styles if invalid input
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        {/* dynamic conditional inline styles */}
+        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} style={{ borderColor: !isValid ? "red" : "black", background: !isValid ? "salmon" : "transparent" }} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
